0f59922c1aecdbd63e06b66086a19b18cf3b1a58
Bjolverk <Bjolverk@gmail.com>
2025-07-03 16:56:22 +0300

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,42 +1,45 @@
 from playwright.sync_api import sync_playwright, expect
 
+TEST_DATA = {
+    "email": "user.name@gmail.com",
+    "username": "username",
+    "password": "password"
+}
+
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
+
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
     email_input = page.get_by_test_id("registration-form-email-input").locator("input")
-    expect(email_input).to_be_visible()
 
     username_input = page.get_by_test_id("registration-form-username-input").locator("input")
-    expect(username_input).to_be_visible()
 
     password_input = page.get_by_test_id("registration-form-password-input").locator("input")
-    expect(password_input).to_be_visible()
 
     registration_button = page.get_by_test_id("registration-page-registration-button")
-    expect(registration_button).to_be_visible()
-    expect(registration_button).to_be_disabled()
 
-    email_input.fill("user.name@gmail.com")
-    expect(email_input).to_have_value("user.name@gmail.com")
-    expect(registration_button).to_be_disabled()
+    for field in [email_input, username_input, password_input]:
+        expect(field).to_be_visible()
+        expect(field).to_be_editable()
+        expect(field).to_be_empty()
 
-    username_input.fill("username")
-    expect(username_input).to_have_value("username")
-    expect(registration_button).to_be_disabled()
-
-    password_input.fill("password")
-    expect(password_input).to_have_value("password")
-    expect(registration_button).to_be_enabled()
+    email_input.fill(TEST_DATA['email'])
+    username_input.fill(TEST_DATA['username'])
+    password_input.fill(TEST_DATA['password'])
 
     registration_button.click()
 
-    expect(page).to_have_url("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+    context.storage_state(path='browser-state.json')
 
-    dashbord_label = page.get_by_test_id("dashboard-toolbar-title-text")
-    expect(dashbord_label).to_have_text("Dashboard")
-
-
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+    # page = browser.new_page()
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
 
     page.wait_for_timeout(5000)
+
